2db86c4a8893ce7b6c9241dc0db4a2b1a51886c1
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-10-01 14:49:12 -0400

    Add project files.

---
+++ W5IntroToWpf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace W5IntroToWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Hello world", "Message", MessageBoxButton.YesNo, MessageBoxImage.Information);
+
+            if (result == MessageBoxResult.Yes)
+                MessageBox.Show("YES clicked");
+            else
+                MessageBox.Show("NO clicked");
+        }
+    }
+}
